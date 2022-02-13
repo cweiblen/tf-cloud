23dc8f5924fe10c952307e13dddfa0f6ee4c0934
Charlie Weiblen <charlie@weiblen.com>
2022-02-12 22:09:35 -0500

    tf updates

--- main.tf
+++ main.tf
@@ -6,8 +6,18 @@ terraform {
       name = "github-dev"
     }
   }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
 }
 
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
